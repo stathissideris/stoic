41eabd77687cdd5132b28cd3dbb80ac895627576
Jon Pither <jon.pither@gmail.com>
2014-04-04 14:08:49 +0000

    Initial pass

--- test/stoic/bootstrap_test.clj
+++ test/stoic/bootstrap_test.clj
@@ -6,14 +6,16 @@
             [com.stuartsierra.component :as component]
             [stoic.bootstrap :as b]))
 
-(defrecord TestComponent [starts stops settings]
+(defrecord TestComponent [starts stops]
   component/Lifecycle
 
-  (start [component]
+  (start [{:keys [settings]}]
     (>!! starts (:a @settings)))
 
-  (stop [component]
+  (stop [{:keys [settings]}]
     (>!! stops (:a @settings))))
+
+;; TODO test components with dependencies get injectedyo
 
 (deftest can-bounce-component-on-config-change
   (let [starts (chan (buffer 1))
@@ -21,7 +23,10 @@
         client (connect)]
     (add-to-zk client (path-for :default :test) {:a :initial-value})
 
-    (b/bootstrap [[:test (fn [s] (->TestComponent starts stops s))]])
+    (component/start
+     (b/bootstrap
+      (component/system-map :foo (->TestComponent starts stops))))
+
     (is (= :initial-value (first (alts!! [(timeout 2000) starts]))))
 
     (add-to-zk client (path-for :default :test) {:a :b})
